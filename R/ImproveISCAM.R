2ea3d81ea5cb1a788bc3605f496ef5b68ce53753
Jung  Dong Won <jungd@mathR.math.appstate.edu>
2016-04-18 20:23:58 -0400

    Clean up

--- R/ImproveISCAM.R
+++ R/ImproveISCAM.R
@@ -3,6 +3,6 @@
 #' @docType package
 #' @name ImproveISCAM-package
 #' @title The most incredible editable ImproveISCAM package
-#' @section For future student will use little better code for Dr.Arnholt class.
+#' @section Indy: This is not working but you will make it work.
 #' @keywords package
 NULL
--- vignettes/ISCAM.R
+++ vignettes/ISCAM.R
@@ -1,7 +1,32 @@
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
+## ---- label = "SETUP", echo = FALSE, results= 'hide', message = FALSE, warning = FALSE----
+library(knitr)
+knitr::opts_chunk$set(comment = NA, fig.align = 'center', fig.height = 5, fig.width = 5, prompt = FALSE, 
+highlight = TRUE, tidy = FALSE, warning = FALSE, message = FALSE, 
+tidy.opts=list(blank = TRUE, width.cutoff = 80))
 
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
+## ------------------------------------------------------------------------
+library(ImproveISCAM)
+binomprob(3,20,1/3,lower.tail = FALSE)
+binomprob(3,20,1/3,lower.tail = TRUE)
+
+## ------------------------------------------------------------------------
+binomtest(3, 13, 1/2, "greater")
+
+## ---- fig.width= 7, fig.height= 7----------------------------------------
+binomtest(3, 13, conf.level=c(90,95))
+
+
+## ------------------------------------------------------------------------
+normprob(2, direction="above")
+
+## ------------------------------------------------------------------------
+InNorm(0.88, direction="above")
+InNorm(0.88, direction="between")
+
+## ---- fig.width= 5, fig.height=7-----------------------------------------
+propztest(2.67, 18, 1/3, "greater")
+
+
+## ---- fig.width=7, fig.height=7------------------------------------------
+propztest(3, 13, conf.level=c(90,95))
 
